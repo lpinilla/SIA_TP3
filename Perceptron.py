60d7777477e065d46b8c34eac6a741ac01bbc8a4
Lautaro Pinilla <lpinilla@itba.edu.ar>
2020-09-19 12:48:30 -0300

    agregados comentarios

--- Perceptron.py
+++ Perceptron.py
@@ -1,12 +1,16 @@
 import random
 import numpy as np
 
+#cantidad de iteraciones maximas para entrenar
 max_steps = 1000
+#eta adaptativo
+#hasta cuantos errores vamos a mirar si variamos eta
 error_window = 4
 eta_variation = 0.001
 
 class Perceptron:
 
+    #constructor del perceptrón
     def __init__(self, input_size, eta, \
                  activation_fun, deriv_fun, split_data=False, test_p=1):
         global max_steps
@@ -21,15 +25,15 @@ class Perceptron:
         self.test_p = test_p
         self.errors = [10000 for i in range(0, error_window)]
 
+    #función para generar nuevos pesos al azar
     def random_weights(self):
         return np.array(
             [random.random() * 2 - 1 for i in range(1, self.input_size + 2)]
         )
 
-    #agregar un 1 al valor y devolver un numpy array
+    #agregar un 1 al valor (para el sesgo) y devolver un numpy array
     def process_input(self, input_arr, expected_arr):
-        #checkear que la cantidad de inputs sea la misma que
-        #la cantidad de pesos
+        #checkear que #inputs sea igual a #pesos
         for inp in input_arr:
             assert len(self.weights) == len(inp) + 1
         inputs = []
@@ -51,25 +55,32 @@ class Perceptron:
                    expected_arr[split_idx:]
         return np.array(inputs), expected_arr, np.array(inputs), expected_arr
 
+    #función para calcular el error
     def calculate_error(self, test_data, expected_values):
+        #calcular los valores de excitación (el h)
         exts = np.array([np.dot(self.weights, i) for i in test_data])
+        #calcular los valores de activación (el O)
         acts = np.array([self.activation_fun(i) for i in exts])
         error = 0.5 * ((expected_values - acts)**2).sum()
+        #eta adaptativo, ir guardando los errores
         self.errors.append(error)
         self.errors = self.errors[1:]
         return error
 
+    #función para calcular el nuevo delta w
     def calculate_deltaW(self, sample_expected, sample, ext):
         return self.eta * (sample_expected - self.activation_fun(ext)) \
                * self.deriv_fun(ext) * sample.transpose()
 
     #función de activación
     def guess(self, input_arr):
+        #agregar un 1 al final del input del usuario, el sesgo
         inp = input_arr
         inp.append(1)
         inp = np.array(inp)
         return self.activation_fun(np.dot(self.weights, inp))
 
+    #función para hacer el eta adaptativo
     def adapt_eta(self, last_error):
         bigger = all(last_error >= i for i in self.errors)
         smaller = all(last_error < i for i in self.errors)
@@ -79,19 +90,22 @@ class Perceptron:
             self.eta += eta_variation
 
 
+    #función de entrenamiento, entrena sobre una tanda de archivos y calcula
+    #el error en base a la tanda de prueba
     def train(self, inputs, expected):
-        #pasar de una lista a un array de numpy
+        #pasar de una lista a un array de numpy para facilitar cuentas
         inp_data, inp_exp, test_data, test_exp = \
                 self.process_input(inputs, expected)
+        #incializando valores
         error = 1
         curr_step = 0
         error_min = 1
         min_weights = self.weights
         while error > 0 and curr_step != max_steps:
             #si no encontramos solución en 100 pasos, reiniciar pesos
-            if curr_step % 1000:
+            if curr_step % 100:
                 self.weights = self.random_weights()
-            #agarramos una muestra al azar con su valor experado
+            #agarramos una muestra al azar del batch con su valor experado
             idx = random.randint(0, len(inp_data) -1)
             sample = inp_data[idx]
             s_exp = inp_exp[idx]
@@ -101,7 +115,10 @@ class Perceptron:
             self.weights += self.calculate_deltaW(s_exp, sample, ext)
             #calcular el error evaluando todos los datos
             error = self.calculate_error(test_data, test_exp)
-            self.adapt_eta(error)
+            #opcional/experimental: llamar a la función para variar eta
+            #en base a cómo es el error en comparación a cómo venía dando
+            #self.adapt_eta(error)
+            #guardamos el error mínimo y sus pesos para devolverlo más tarde
             if error < error_min:
                 error_min = error
                 min_weights = self.weights
--- ejer2.py
+++ ejer2.py
@@ -7,6 +7,7 @@ learning_rate = 0.01
 
 #Ejercicio 2
 
+#donde se encuentran los datasets
 datasets_basepath = "ej2_resources/"
 
 #activación lineal
@@ -17,7 +18,7 @@ def linear_deriv(x):
     return 1
 
 beta = 0.5 #TODO: ver que valor poner
-
+#activación no lineal y su derivada
 def no_linear_activation(x):
     return 1 / (1 + np.exp(-2 * beta * x))
 
@@ -26,37 +27,32 @@ def no_linear_deriv(x):
     return 2 * beta * act * (1 - act)
 
 #Creamos perceptron lineal que dividiendo el dataset en 10% para testeo
-
 p = Perceptron(3, learning_rate, activation_fun=linear_activation, deriv_fun=linear_deriv, split_data=True, test_p=0.1)
 
+#Creamos perceptron no-lineal que dividiendo el dataset en 10% para testeo
 #p = Perceptron(3, learning_rate, activation_fun=no_linear_activation, deriv_fun=no_linear_deriv, split_data=True, test_p=0.1)
 
 error = 5
+#indicar en cuantas partes está dividido el dataset
 n_of_parts = 4
 
 while error > 0:
     #Agarramos un dataset random
     f = open(datasets_basepath + str(random.randint(0,n_of_parts - 1)) + ".pickle", "rb")
+    #cargar los valores de entrada
     inp = pickle.load(f)
+    #cargar los valores esperados
     exp = pickle.load(f)
     f.close()
+    #entrenar con este dataset
     iterations, weights, min_weights, error = p.train(inp, exp)
     print(error)
 
+#Testear al perceptrón
 f = open(datasets_basepath + str(random.randint(0,n_of_parts - 1)) + ".pickle", "rb")
 inp = pickle.load(f)
 exp = pickle.load(f)
-
+f.close()
 for i in range(0, len(inp)):
     print("Expected " + str(exp[i]))
     print("Result " + str(p.guess(inp[i])))
-
-#activacion no lineal
-#beta = 0.7 #TODO: ver que valor poner
-#
-#def no_linear_activation(x):
-#    return 1 / (1 + np.exp(-2 * beta * x))
-#
-#def no_linear_deriv(x):
-#    act = no_linear_activation(x)
-#    return 2 * beta * act * (1 - act)
