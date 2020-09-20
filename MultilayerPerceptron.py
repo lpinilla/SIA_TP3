9f526b2d5cfa3dee6a0d51668bc0a473b311a9b0
Lautaro Pinilla <lpinilla@itba.edu.ar>
2020-09-19 23:35:25 -0300

    Función para calcular error en perceptrón multicapa y siguiendo arreglando cosas

--- MultilayerPerceptron.py
+++ MultilayerPerceptron.py
@@ -1,12 +1,15 @@
 import random
+from math import log
 import numpy as np
 
 layers = []
 
 class MultilayerPerceptron:
 
-    def __init__(self ):
+    def __init__(self, eta, momentum):
         global layers
+        self.eta = eta
+        self.momentum = momentum
 
     #función para generar un array con valores al azar
     #introducimos un valor de más para el sesgo
@@ -20,6 +23,8 @@ class MultilayerPerceptron:
         layer = {
             #pesos
             "w" : self.random_array(n_of_nodes),
+            #pesos anteriores, para usar momentum
+            "prev_w" : self.random_array(n_of_nodes),
             #valores de activación
             "v" : self.random_array(n_of_nodes),
             #valores de exitación
@@ -33,6 +38,28 @@ class MultilayerPerceptron:
         }
         layers.append(layer)
 
+    def guess(self, _input):
+        _data = _input
+        _data.append(1)
+        data = np.array(_data)
+        layers[0]["v"] = data
+        feed_forward(self)
+        return layers[len(layers)-1]["v"]
+
+    def calculate_error(self, test_data, expected):
+        guesses = [guess(i) for i in test_data]
+        aux = 0
+        for i in range(0, len(expected)):
+            exp = expected[i]
+            for j in range(0, len(exp)):
+                e_1 = 1 + exp[j]
+                e__1 = 1 - exp[j]
+                o_1 = 1 + guesses[i][j]
+                o__1 = 1 - guesses[i][j]
+                aux += (e_1 * log(e_1 / o_1) + e__1 * log(e__1, o__1))
+        return 0.5 * aux
+
+
     #función para propagar secuencialmente los valores
     def feed_forward(self):
         for i in range(1, len(layers)):
@@ -41,15 +68,20 @@ class MultilayerPerceptron:
             l["v"] = l["fn"](l["h"])
 
     #función que propaga regresivamente el valor de error
-    def back_propagation(self):
+    def back_propagation(self, inputs):
         for i in range(len(layers)-1, 2, -1):
             l = layers[i]
             l_1 = layers[i+1]
             aux = np.dot(l_1["weights"].transpose(), l_1["errors"])
             l["errors"] = [l["deriv"](j) for j in aux]
-
-    def calculate_deltaW(self):
-        return 1 #FIXME
+            #actualizar los pesos
+            w = l["prev_w"]
+            l["weights"] += self.eta * \
+                np.dot(l["errors"].transpose(), layers[i-1]["v"]) + momentum * w
+        #falta ajustar los valores para la capa numero 1 que interactua con el input
+        l = layers[1]
+        w = l["prev_w"]
+        l["weights"] += self.eta * np.dot(l[2]["errors"], inputs) + momentum * w
 
     def train(self,inp_data, exp_data):
         #TODO implementar
