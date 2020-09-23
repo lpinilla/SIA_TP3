1d82f7bec02e38ec60c7dcf438b5fbaf92662c58
fermartin17 <femartin@itba.edu.ar>
2020-09-23 17:43:01 -0300

    testing

--- ejer3/digit_recongnizer.py
+++ ejer3/digit_recongnizer.py
@@ -9,20 +9,19 @@ _input = pickle.load(f)
 _expected = pickle.load(f)
 f.close()
 
-learning_rate = 0.001
-momentum = 0.8
+learning_rate = 1.5
+momentum = 1
 test_percentage = 0.1
 
 #definimos la función de activación no lineal entre 0 y 1 (porque estamos buscando probabilidades)
 
-beta = 0.8 #TODO: ver que valor poner
+beta = 0.01 #TODO: ver que valor poner
 #activación no lineal y su derivada
 def logistic(x):
     return 1 / (1 + np.exp(-2 * beta * x))
 
 def logistic_deriv(x):
-    #act = logistic(x)
-    act = x
+    act = logistic(x)
     return 2 * beta * act * (1 - act)
 
 def tanh(x):
@@ -48,7 +47,7 @@ nn = MultilayerPerceptron(learning_rate, momentum, act_fun=arctan, deriv_fun=arc
 #10 nodos de salida en donde indicamos la probabilidad de que sea ese número
 nn.entry_layer(35)
 #capa oculta intermedia
-nn.add_hidden_layer(100, logistic, logistic_deriv)
+nn.add_hidden_layer(70, logistic, logistic_deriv)
 #la salida
 nn.output_layer(10)
 
